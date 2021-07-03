0a389965cbb16d5d46fc6215dc96d112a0725025
24k199706 <24k0626ryo@gmail.com>
2021-07-03 19:59:54 +0900

    任意のメッセージ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,33 @@
 class PostsController < ApplicationController
   def index
-    @posts=Post.all
+    @posts=Post.all.order(created_at: :desc)
   end
   def show
     @post= Post.find_by(id: params[:id])
   end
+  
   def new
     
+  end
+
+  def create
+    @post=Post.new(content: params[:content])
+    @post.save
+    redirect_to index_path
   end
 
   def edit
     @post=Post.find_by(id: params[:id])
   end
   def update
-    redirect_to("index")
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to index_path
+    end
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get "posts/:id/edit",to: "posts#edit"
-  post "posts/:id/update",to: "posts#update"
-  get "posts/new",to: "posts#new"
-  get 'posts/:id',to: "posts#show"
-  get 'index',to: "posts#index"
-  get '/',to: "home#top"
-  get "about",to: "home#about"
+  get "posts/index",to: "posts#index", as: "index"
+  get "posts/new",to: "posts#new", as: "new"
+  get "posts/:id",to: "posts#show",as: "show"
+  post "posts/create", to:"posts#create", as:"create"
+  get "posts/:id/edit",to: "posts#edit",as:"edit"
+  post "posts/:id/update",to: "posts#update",as:"update" 
+  root "home#top" ,as:"home"
+  get "about",to: "home#about",as:"about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
